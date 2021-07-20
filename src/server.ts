c45eb8bcda76c36b944f84c26c809397850a2405
ThaSMorato <tha.s.morato@gmail.com>
2021-07-20 19:21:55 -0300

    aula-3 - Tags and (wip) admin middleware

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,18 @@
 import "reflect-metadata";
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
+import "express-async-errors";
 import {  router  } from "./routes";
+import { errorHandler } from "./middlewares/errorHandler";
 
 import "./database";
 
 const app = express();
 
 app.use(express.json());
+
 app.use( router );
+
+app.use( errorHandler )
 
 app.listen(3000, () => {
     console.log("SERVER IS RUNNING");
