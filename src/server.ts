9d6363e0391e4315439b476ab2a7b5dcc4972d92
ThaSMorato <tha.s.morato@gmail.com>
2021-07-20 10:28:19 -0300

    aula-1 and init project

--- src/server.ts
+++ src/server.ts
@@ -6,6 +6,10 @@ app.get("/teste", (request, response) => {
     return response.send("ola nlw");
 })
 
+app.post("/teste", (request, response) => {
+    
+})
+
 app.listen(3000, () => {
     console.log("SERVER IS RUNNING");
 });
